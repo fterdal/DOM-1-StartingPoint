b9bdbf6c26e899824969a3f685667197cc6d42b5
tabularasae <tabularasae@icloud.com>
2025-06-18 12:08:41 -0400

    Added console.log printouts

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 console.log("Hello! If you see this, the script is working.");
 
 // - [ 1 ] Select the section with an id of container without using querySelector.
+console.log("Feature 1");
 
 // - [ 2 ] Select the section with an id of container using querySelector.
+console.log("Feature 2");
 
 // - [ 3 ] Select all of the list items with a class of "second".
+console.log("Feature 3");
 
 // - [ 4 ] Select a list item with a class of third, but only the list item inside of the ol tag.
+console.log("Feature 4");
 
 // - [ 5 ] Give the section with an id of container the text "Hello!".
+console.log("Feature 5");
 
 // - [ 6 ] Add the class main to the div with a class of footer.
+console.log("Feature 6");
 
 // - [ 7 ] Remove the class main on the div with a class of footer.
+console.log("Feature 7");
 
 // - [ 8 ] Create a new li element.
+console.log("Feature 8");
 
 // - [ 9 ] Give the li the text "four".
+console.log("Feature 9");
 
 // - [ 10 ] Append the li to the ul element.
+console.log("Feature 10");
 
 // - [ 11 ] Loop over all of the lis inside the ol tag and give them a background color of "green".
+console.log("Feature 11");
 
 // - [ 12 ] Remove the div with a class of footer.
-
+console.log("Feature 12");
 
 // Try rewriting this without using querySelector
 const header = document.querySelector("#container");
