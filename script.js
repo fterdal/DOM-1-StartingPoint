a4e4614622c7cd901258fe45281bc86ec328cdd3
Michelangelo <Michael.r.javier@gmail.com>
2025-06-18 13:26:52 -0400

    Initial commit for problems

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log("Hello! If you see this, the script is working.");
 
+console.log("Problem 1");
+
 /*
+
 - [ ] Select the section with an id of container without using querySelector.
 - [ ] Select the section with an id of container using querySelector.
 - [ ] Select all of the list items with a class of "second".
 - [ ] Select a list item with a class of third, but only the list item inside of the ol tag.
-
+/*
 - [ ] Give the section with an id of container the text "Hello!".
 - [ ] Add the class main to the div with a class of footer.
 - [ ] Remove the class main on the div with a class of footer.
@@ -18,5 +21,26 @@ console.log("Hello! If you see this, the script is working.");
 */
 
 // Try rewriting this without using querySelector
-const header = document.querySelector("#container");
-console.log("header", header);
+const header = document.getElementById("header");
+header.textContent = "Hello!";
+
+const container = document.getElementById("container");
+container.textContent = "Hello!";
+
+//Selecting section with id of container using querySelector
+console.log("Problem 2");
+
+const containerQuery = document.querySelector("#container");
+console.log("containerQuery", containerQuery);
+
+// Selecting all list items with a class of "second"
+console.log("Problem 3");
+
+const secondItems = document.getElementsByClassName("second");
+console.log("secondItems", secondItems);
+
+// Selecting a list item with a class of third, but only the list item inside of the ol tag
+console.log("Problem 4");
+
+const thirdItem = document.querySelector("ol .third");
+console.log("thirdItem", thirdItem);
