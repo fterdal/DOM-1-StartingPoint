6681338962e8db7b899508c5b66c79e40ee01dea
Olivia Wilson-Simmonds <heavenwilson210@gmail.com>
2025-06-18 12:21:48 -0400

    Questions 10 - 12, and bonus

--- script.js
+++ script.js
@@ -2,19 +2,66 @@ console.log("Hello! If you see this, the script is working.");
 
 /*
 - [ ] Select the section with an id of container without using querySelector.
+
+
+
+
+
 - [ ] Select the section with an id of container using querySelector.
+
+
+
+
 - [ ] Select all of the list items with a class of "second".
+
+
+
+
 - [ ] Select a list item with a class of third, but only the list item inside of the ol tag.
+
+
+
+
 - [ ] Give the section with an id of container the text "Hello!".
+
+
+
+
 - [ ] Add the class main to the div with a class of footer.
+
+
+
+
 - [ ] Remove the class main on the div with a class of footer.
+
+
+
+
 - [ ] Create a new li element.
-- [ ] Give the li the text "four".
-- [ ] Append the li to the ul element.
-- [ ] Loop over all of the lis inside the ol tag and give them a background color of "green".
-- [ ] Remove the div with a class of footer.
-*/
+
+
+
+
+- [ ] Give the li the text "four".*/
+
+
+
+
+/*- [ ] Append the li to the ul element.*/
+const ulElement = document.querySelector("ul");
+ulElement.appendChild(newLi);
+
+
+/*- [ ] Loop over all of the lis inside the ol tag and give them a background color of "green".*/
+const olGrn = document.querySelectorAll("ol li");
+olGrn.forEach(li => {
+  li.style.backgroundColor = "green";
+});
+/*- [ ] Remove the div with a class of footer.*/
+footerDiv.remove();
+
+
 
 // Try rewriting this without using querySelector
-const header = document.querySelector("#container");
+const header = document.getElementById("container");
 console.log("header", header);
