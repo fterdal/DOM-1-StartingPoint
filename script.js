f131a27b87a7a86f51d5ea04133fed6085a3faca
Jocsan Rodriguez <jasub270@gmail.com>
2025-06-18 13:08:11 -0400

    x markers

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ console.log("Hello! If you see this, the script is working.");
 - [ ] Select a list item with a class of third, but only the list item inside of the ol tag.
 - [ ] Give the section with an id of container the text "Hello!".
 - [ ] Add the class main to the div with a class of footer.
-- [ ] Remove the class main on the div with a class of footer.
-- [ ] Create a new li element.
-- [ ] Give the li the text "four".
+- [x] Remove the class main on the div with a class of footer.
+- [x] Create a new li element.
+- [x] Give the li the text "four".
 - [ ] Append the li to the ul element.
 - [ ] Loop over all of the lis inside the ol tag and give them a background color of "green".
 - [ ] Remove the div with a class of footer.
@@ -26,6 +26,7 @@ console.log("Section (Using Query Selector): ", sectionWithQuery);
 // Select all of the list items with a class of "second".
 const secondItems = document.querySelectorAll("li.second");
 console.log("List items with class 'second': ", secondItems);
+
 // Remove the class main on the div with a class of footer.
 footerDiv.classList.remove('main');
 
